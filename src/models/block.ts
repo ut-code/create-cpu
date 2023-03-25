237acfe8a345f7112ab531f8485cb036a72aa540
taka2 <taka23131@gmail.com>
2023-03-25 21:30:42 +0900

    CCBlock側でblockTypeプロパティを持つようにした。blockTypeによってrenderメソッドの処理が分岐する

--- src/models/block.ts
+++ src/models/block.ts
@@ -6,6 +6,8 @@ export type CCBlockRegistrationProps = {
   onDragStart(e: PIXI.FederatedMouseEvent): void;
 };
 
+export type BlockType = "And" | "Custom";
+
 export default class CCBlock {
   // #props?: CCBlockRegistrationProps;
 
@@ -13,14 +15,21 @@ export default class CCBlock {
 
   #position: PIXI.Point;
 
+  #wireLength = 30;
+
   #size = new PIXI.Point(100, 100);
 
   #isSelected = false;
 
-  constructor(position: PIXI.IPointData) {
+  constructor(position: PIXI.IPointData, public blockType: BlockType) {
     this.#position = new PIXI.Point(position.x, position.y);
     this.#pixiGraphics = new PIXI.Graphics();
     this.#pixiGraphics.interactive = true;
+    if (blockType === "Custom") {
+      this.#size = new PIXI.Point(100, 100);
+    } else if (blockType === "And") {
+      this.#size = new PIXI.Point(70, 100);
+    }
   }
 
   register(props: CCBlockRegistrationProps) {
@@ -37,32 +46,144 @@ export default class CCBlock {
   private render() {
     const borderWidth = 5;
     const outlineWidth = 3;
-    this.#pixiGraphics.clear();
-    this.#pixiGraphics.beginFill(whiteColor);
-    this.#pixiGraphics.lineStyle({
-      color: blackColor,
-      width: borderWidth,
-      alignment: 1,
-    });
-    this.#pixiGraphics.drawRect(
-      this.#position.x - this.#size.x / 2,
-      this.#position.y - this.#size.y / 2,
-      this.#size.x,
-      this.#size.y
-    );
-    this.#pixiGraphics.endFill();
-    if (this.#isSelected) {
-      this.#pixiGraphics.lineStyle({
-        color: primaryColor,
-        width: outlineWidth,
-        alignment: 1,
-      });
-      this.#pixiGraphics.drawRect(
-        this.#position.x - this.#size.x / 2 - borderWidth,
-        this.#position.y - this.#size.y / 2 - borderWidth,
-        this.#size.x + borderWidth * 2,
-        this.#size.y + borderWidth * 2
-      );
+    switch (this.blockType) {
+      case "Custom": {
+        this.#pixiGraphics.clear();
+        this.#pixiGraphics.beginFill(whiteColor);
+        this.#pixiGraphics.lineStyle({
+          color: blackColor,
+          width: borderWidth,
+          alignment: 1,
+        });
+        this.#pixiGraphics.drawRect(
+          this.#position.x - this.#size.x / 2,
+          this.#position.y - this.#size.y / 2,
+          this.#size.x,
+          this.#size.y
+        );
+        this.#pixiGraphics.endFill();
+        if (this.#isSelected) {
+          this.#pixiGraphics.lineStyle({
+            color: primaryColor,
+            width: outlineWidth,
+            alignment: 1,
+          });
+          this.#pixiGraphics.drawRect(
+            this.#position.x - this.#size.x / 2 - borderWidth,
+            this.#position.y - this.#size.y / 2 - borderWidth,
+            this.#size.x + borderWidth * 2,
+            this.#size.y + borderWidth * 2
+          );
+        }
+        break;
+      }
+      case "And": {
+        const radius = this.#size.y / 2;
+        const wirePositionFromEdge = 25;
+        const rectangleLeftTop = {
+          x: this.#position.x - this.#size.x / 2,
+          y: this.#position.y - this.#size.y / 2,
+        };
+        const rectangleLeftBottom = {
+          x: this.#position.x - this.#size.x / 2,
+          y: this.#position.y + this.#size.y / 2,
+        };
+        const rectangleRightTop = {
+          x: this.#position.x + this.#size.x / 2,
+          y: this.#position.y - this.#size.y / 2,
+        };
+        this.#pixiGraphics.clear();
+        this.#pixiGraphics.beginFill(whiteColor);
+        this.#pixiGraphics.drawRect(
+          rectangleLeftTop.x,
+          rectangleLeftTop.y,
+          this.#size.x,
+          this.#size.y
+        );
+        this.#pixiGraphics.drawCircle(
+          this.#position.x + this.#size.x / 2,
+          this.#position.y,
+          radius
+        );
+        this.#pixiGraphics.endFill();
+        this.#pixiGraphics.lineStyle({
+          color: blackColor,
+          width: borderWidth,
+          alignment: 1,
+        });
+        this.#pixiGraphics.moveTo(rectangleLeftTop.x, rectangleLeftTop.y);
+        this.#pixiGraphics.lineTo(rectangleRightTop.x, rectangleRightTop.y);
+        this.#pixiGraphics.arc(
+          this.#position.x + this.#size.x / 2,
+          this.#position.y,
+          radius,
+          -Math.PI / 2,
+          Math.PI / 2
+        );
+        this.#pixiGraphics.lineTo(rectangleLeftBottom.x, rectangleLeftBottom.y);
+        this.#pixiGraphics.lineTo(
+          rectangleLeftTop.x,
+          rectangleLeftTop.y - borderWidth
+        );
+        this.#pixiGraphics.moveTo(
+          rectangleLeftBottom.x,
+          rectangleLeftBottom.y - wirePositionFromEdge
+        );
+        this.#pixiGraphics.lineTo(
+          rectangleLeftBottom.x - this.#wireLength,
+          rectangleLeftBottom.y - wirePositionFromEdge
+        );
+        this.#pixiGraphics.moveTo(
+          rectangleLeftTop.x,
+          rectangleLeftTop.y + wirePositionFromEdge
+        );
+        this.#pixiGraphics.lineTo(
+          rectangleLeftTop.x - this.#wireLength,
+          rectangleLeftTop.y + wirePositionFromEdge
+        );
+        this.#pixiGraphics.moveTo(
+          this.#position.x + this.#size.x / 2 + radius,
+          this.#position.y
+        );
+        this.#pixiGraphics.lineTo(
+          this.#position.x + this.#size.x / 2 + radius + this.#wireLength,
+          this.#position.y
+        );
+        if (this.#isSelected) {
+          this.#pixiGraphics.lineStyle({
+            color: primaryColor,
+            width: outlineWidth,
+            alignment: 1,
+          });
+          this.#pixiGraphics.moveTo(
+            rectangleLeftTop.x - borderWidth,
+            rectangleLeftTop.y - borderWidth
+          );
+          this.#pixiGraphics.lineTo(
+            rectangleRightTop.x,
+            rectangleRightTop.y - borderWidth
+          );
+          this.#pixiGraphics.arc(
+            this.#position.x + this.#size.x / 2,
+            this.#position.y,
+            this.#size.y / 2 + borderWidth,
+            -Math.PI / 2,
+            Math.PI / 2
+          );
+          this.#pixiGraphics.lineTo(
+            rectangleLeftBottom.x - borderWidth,
+            rectangleLeftBottom.y + borderWidth
+          );
+          this.#pixiGraphics.lineTo(
+            rectangleLeftTop.x - borderWidth,
+            rectangleLeftTop.y - borderWidth - outlineWidth
+          );
+        }
+        break;
+      }
+      default: {
+        break;
+      }
     }
   }
 
