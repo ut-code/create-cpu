c8cf5a810efea63f5613d052a9b18c4b7a316145
taka2 <taka23131@gmail.com>
2023-04-05 08:52:12 +0900

    Implementation of the basic display of components.

--- src/models/block.ts
+++ src/models/block.ts
@@ -9,6 +9,7 @@ export type CCBlockRegistrationProps = {
 
 type PixiTexts = {
   componentName: PIXI.Text;
+  edgesName: Map<string, PIXI.Text>;
 };
 
 export default class CCBlock {
@@ -18,7 +19,11 @@ export default class CCBlock {
 
   #position: PIXI.Point;
 
-  #size = new PIXI.Point(100, 100);
+  #size = new PIXI.Point(200, 100);
+
+  #componentNameFontSize = 24;
+
+  #edgenameFontSize = 16;
 
   #isSelected = false;
 
@@ -32,15 +37,33 @@ export default class CCBlock {
     this.#pixiGraphics.interactive = true;
     this.#componentDefinition = component;
     const componentName = new PIXI.Text(this.#componentDefinition.name, {
-      fontSize: 24,
+      fontSize: this.#componentNameFontSize,
     });
-    this.#pixiTexts = { componentName };
+    const map = new Map<string, PIXI.Text>();
+    for (const edge of component.inputEdges) {
+      map.set(
+        edge.id,
+        new PIXI.Text(edge.name, { fontSize: this.#edgenameFontSize })
+      );
+    }
+    for (const edge of component.outputEdges) {
+      map.set(
+        edge.id,
+        new PIXI.Text(edge.name, {
+          fontSize: this.#edgenameFontSize,
+        })
+      );
+    }
+    this.#pixiTexts = { componentName, edgesName: map };
   }
 
   register(props: CCBlockRegistrationProps) {
     // this.#props = props;
     props.pixiContainer.addChild(this.#pixiGraphics);
     props.pixiContainer.addChild(this.#pixiTexts.componentName);
+    this.#pixiTexts.edgesName.forEach((value) =>
+      props.pixiContainer.addChild(value)
+    );
     this.#pixiGraphics.on("mousedown", (e) => {
       this.isSelected = true;
       props.onDragStart(e);
@@ -67,41 +90,60 @@ export default class CCBlock {
     );
     const inputEdgeGap =
       this.#size.y / (this.#componentDefinition.inputEdges.length + 1);
+    const gap = 6;
     const edgeSize = 10;
     this.#componentDefinition.inputEdges.forEach((edge, index) => {
-      this.#pixiGraphics.drawRoundedRect(
-        this.#position.x - this.#size.x / 2 - edgeSize / 2,
-        this.#position.y -
+      const position = {
+        x: this.#position.x - this.#size.x / 2 - edgeSize / 2,
+        y:
+          this.#position.y -
           this.#size.y / 2 +
           inputEdgeGap * (index + 1) -
           edgeSize / 2,
+      };
+      this.#pixiGraphics.drawRoundedRect(
+        position.x,
+        position.y,
         edgeSize,
         edgeSize,
         2
       );
+      const edgeName = this.#pixiTexts.edgesName.get(edge.id);
+      if (edgeName) {
+        edgeName.x = position.x + edgeSize + gap;
+        edgeName.y = position.y;
+        edgeName.anchor.set(0, 0.25);
+      }
     });
     const outputEdgeGap =
       this.#size.y / (this.#componentDefinition.outputEdges.length + 1);
     this.#componentDefinition.outputEdges.forEach((edge, index) => {
-      this.#pixiGraphics.drawRoundedRect(
-        this.#position.x + this.#size.x / 2 - edgeSize / 2,
-        this.#position.y -
+      const position = {
+        x: this.#position.x + this.#size.x / 2 - edgeSize / 2,
+        y:
+          this.#position.y -
           this.#size.y / 2 +
           outputEdgeGap * (index + 1) -
           edgeSize / 2,
+      };
+      this.#pixiGraphics.drawRoundedRect(
+        position.x,
+        position.y,
         edgeSize,
         edgeSize,
         2
       );
+      const edgeName = this.#pixiTexts.edgesName.get(edge.id);
+      if (edgeName) {
+        edgeName.x = position.x - gap;
+        edgeName.y = position.y;
+        edgeName.anchor.set(1, 0.25);
+      }
     });
     this.#pixiGraphics.endFill();
+    this.#pixiTexts.componentName.anchor.set(0, 1);
     this.#pixiTexts.componentName.x = this.#position.x - this.#size.x / 2;
-    const gap = 4;
-    this.#pixiTexts.componentName.y =
-      this.#position.y -
-      this.#size.y / 2 -
-      Number(this.#pixiTexts.componentName.style.fontSize) -
-      gap;
+    this.#pixiTexts.componentName.y = this.#position.y - this.#size.y / 2 - gap;
     if (this.#isSelected) {
       this.#pixiGraphics.lineStyle({
         color: primaryColor,
